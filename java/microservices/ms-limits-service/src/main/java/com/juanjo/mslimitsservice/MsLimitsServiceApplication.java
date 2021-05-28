4371c66ffb099b412d409d02fdb930a8542a77f9
santosr09 <santos.r09@gmail.com>
2021-05-28 18:55:21 -0500

    Microservice -limits service

---
+++ java/microservices/ms-limits-service/src/main/java/com/juanjo/mslimitsservice/MsLimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.juanjo.mslimitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLimitsServiceApplication.class, args);
+	}
+
+}
---
+++ java/microservices/ms-limits-service/src/test/java/com/juanjo/mslimitsservice/MsLimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanjo.mslimitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
