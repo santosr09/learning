03452d87150e1f093e9f849b7f838e7633b1c859
santosr09 <santos.r09@gmail.com>
2021-10-31 21:22:26 -0600

    Ratelimiter and CircuitBreaker example

--- java/microservices/currency-exchange-service/src/main/java/com/juanjo/currencyexchangeservice/CircuitBreakerController.java
+++ java/microservices/currency-exchange-service/src/main/java/com/juanjo/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.juanjo.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +18,19 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/failing-sample-api")
 	//@Retry(name = "default")
-	@Retry(name = "sample-api", fallbackMethod = "harcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "harcodedResponse")
+	//@CircuitBreaker(name = "default", fallbackMethod = "harcodedResponse")
+	@RateLimiter(name="default")
+	// 10s => 10,000 calls to this api method
+	@Bulkhead(name="default")
 	public String failingSampleApi() {
 		logger.info("Sample API call received"); //The "retry" will be attemped 3 times by default, before the error
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8001/fake-api", String.class);
 		
-		return forEntity.getBody();
+	/*	ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8001/fake-api", String.class);
+		
+		return forEntity.getBody();*/
+		
+		return "sample-method";
 	}
 	
 	private String harcodedResponse(Exception ex) {
