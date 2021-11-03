fe17c1813f46de3449bb680511f5b0346cb4afb5
santosr09 <santos.r09@gmail.com>
2021-11-02 22:09:46 -0600

    flatMap() example

--- java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/StreamExample.java
+++ java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/StreamExample.java
@@ -8,12 +8,21 @@ public class StreamExample {
 	public static List<String> mapStudentToName() {
 		return StudentDataBase.getAllStudents().stream()
 				.map(Student::getName)
+				.map(String::toUpperCase)
 				.collect(Collectors.toList());
-		
+	}
+	
+	public static List<String> flatMapStudentToActivity() {
+		return StudentDataBase.getAllStudents().stream()
+				.map(Student::getActivities)
+				.flatMap(List::stream)
+				.collect(Collectors.toList());
 	}
 	
 	public static void main(String[] args) {
 	
 		mapStudentToName().stream().forEach(System.out::println);
+		
+		System.out.println(flatMapStudentToActivity());
 	}
 }
