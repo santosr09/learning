612a20a1e0f6f09cb40d3ff28945ab976f4ea5d0
santosr09 <santos.r09@gmail.com>
2021-11-03 08:07:20 -0600

    Examples for distinct(), count() and sorted() method

--- java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/StreamExample.java
+++ java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/StreamExample.java
@@ -1,5 +1,6 @@
 package com.juanjo.ocp.streams_lambdas;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,10 +20,53 @@ public class StreamExample {
 				.collect(Collectors.toList());
 	}
 	
+	public static List<String> distinctActivitesAvailable() {
+		return StudentDataBase.getAllStudents().stream()
+				.map(Student::getActivities)
+				.flatMap(List::stream)
+				.distinct()
+				.collect(Collectors.toList());
+	}
+	
+	public static List<String> distinctActivitesAvailableSorted() {
+		return StudentDataBase.getAllStudents().stream()
+				.map(Student::getActivities)
+				.flatMap(List::stream)
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+	
+	public static Long countActivites() {
+		return StudentDataBase.getAllStudents().stream()
+				.map(Student::getActivities)
+				.flatMap(List::stream)
+				.distinct()
+				.count();
+	}
+	
+	public static List<Student> sortedByNameComparator() {
+		return StudentDataBase.getAllStudents()
+				.stream()
+				.sorted(Comparator.comparing(Student::getName))
+				.collect(Collectors.toList());
+				
+	}
+	
 	public static void main(String[] args) {
 	
 		mapStudentToName().stream().forEach(System.out::println);
 		
 		System.out.println(flatMapStudentToActivity());
+		
+		System.out.println("Activities available for students: " + distinctActivitesAvailable());
+		
+		System.out.println("Activities available sorted BY NATURAL ORDER: " + distinctActivitesAvailableSorted());
+		
+		System.out.println("Total number of activities: " + countActivites());
+		
+		System.out.println("Students sorted by Comparator");
+		System.out.println(sortedByNameComparator());
 	}
+	
 }
