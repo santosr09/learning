b5c8ead6ed9085660182d777f7d22dc779e5382a
santosr09 <santos.r09@gmail.com>
2021-11-07 07:38:46 -0600

    Reduce using map and filter

--- java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/ReduceExStudents.java
+++ java/ocp11/src/main/java/com/juanjo/ocp/streams_lambdas/ReduceExStudents.java
@@ -24,10 +24,34 @@ public class ReduceExStudents {
 				.reduce((s1, s2) -> (s1.getGpa() > s2.getGpa()) ? s1: s2);
 	}
 	
+	//Return the number of notebooks
+	private static int noOfNoteBooks(List<Student> list) {
+		return list.stream()
+				.map(Student::getNoteBooks)
+				//.reduce(0, (a , b) -> a+b);
+		    .reduce(0, Integer::sum);
+	}
+	
+	private static int noOfNoteBooksFiltering(List<Student> list) {
+		return list.stream()
+				.filter((student -> student.getGradeLevel() >=3))
+				.map(Student::getNoteBooks)
+				.reduce(0, Integer::sum);
+	}
+	
 	public static void main(String[] args) {
 		
+		System.out.println("Number of NoteBooks");
+		System.out.println(noOfNoteBooks(students));
+		
+		System.out.println("Number of NoteBooks, when grade level >3");
+		System.out.println(noOfNoteBooksFiltering(students));
+		
+		System.out.println("Get the student with Highest GPA");
 		System.out.println(getHighestGPAStudent(students).get());
 		
+		System.out.println("Get the student with Highest GPA");
 		System.out.println(getHighestGPAStudentSimplified(students).get().getName());
+		
 	}
 }
