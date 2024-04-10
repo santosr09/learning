4f43681d82268764727e03ef18f9265617f8a0d0
santosr09 <santos.r09@gmail.com>
2024-04-09 20:39:35 -0600

    Pushing 'cicd/db_migrations/flyway' folder

---
+++ cicd/db_migrations/spring-boot-flyway/src/test/java/com/jsantos/springbootflyway/SpringBootFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsantos.springbootflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
